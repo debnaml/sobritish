8617a479b74b5d55c68ea83df46ec168189ec4fc
Lee Debnam <lee@pp-worldwide.com>
2023-12-12 12:05:42 +0100

    added time function

--- sobritish.js
+++ sobritish.js
@@ -1,9 +1,30 @@
-const greetings = [];
+const greetings = ["hi", "hello", "hey", "ola", "hallo"];
 const timeBoundGreetings = [];
 const weatherMatrix = [];
-
+const monthsOfYear = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+let currentMonth = "";
+let timePeriod = "";
 
 const findTimeOfDay = () => {
+	const now = new Date();
+	const hours = now.getHours();
+	switch (true) {
+		case hours < 12 && hours > 0 : 
+			timePeriod = "morning";
+			break;
+		case hours >= 12 && hours < 18 : 
+			timePeriod = "afternoon";
+			break;
+		case hours >= 18 && hours < 22 : 
+			timePeriod = "evening";
+			break;
+		default: 
+			timePeriod = "night";
+			break;
+	}
+	currentMonth = monthsOfYear[now.getMonth()];
+	console.log("month = " + currentMonth);
+	console.log("time = " + timePeriod);
 
 }
 
@@ -12,7 +33,19 @@ const checkWeatherIsTypical = () => {
 }
 
 const generateMessage = () => {
-	console.log("Hello");
+	findTimeOfDay();
+	const greetingRandomNum = Math.floor(Math.random() * greetings.length);
+	console.log(greetingRandomNum);
+	console.log(greetings[greetingRandomNum]);
 }
 
-generateMessage();
+
+generateMessage();	
+
+
+
+/* Tests
+	for (let i = 0; i < 1000; i++) {
+		generateMessage();	
+	}
+*/

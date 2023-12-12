503e55db516ce24e2dc013c3bdba673b91a34f4e
Lee Debnam <lee@pp-worldwide.com>
2023-12-12 11:51:15 +0100

    added function stubs js

---
+++ sobritish.js
@@ -0,0 +1,18 @@
+const greetings = [];
+const timeBoundGreetings = [];
+const weatherMatrix = [];
+
+
+const findTimeOfDay = () => {
+
+}
+
+const checkWeatherIsTypical = () => {
+
+}
+
+const generateMessage = () => {
+	console.log("Hello");
+}
+
+generateMessage();

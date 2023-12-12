6a23b12f94c977498df918e65dcf00ad779833f2
Lee Debnam <lee@pp-worldwide.com>
2023-12-12 16:15:34 +0100

    needs tidying

--- sobritish.js
+++ sobritish.js
@@ -1,13 +1,26 @@
 const greetings = ["hi", "hello", "hey", "ola", "hallo"];
 const timeBoundGreetings = [
-	{greeting:"Good Morning", timeperiod:"morning"},
-	{greeting:"Good afternoon", timeperiod:"afternoon"},
-	{greeting:"'Afternoon", timeperiod:"afternoon"},
+	{greeting:"good Morning", timeperiod:"morning"},
+	{greeting:"morning", timeperiod:"morning"},
+	{greeting:"good afternoon", timeperiod:"afternoon"},
+	{greeting:"'afternoon", timeperiod:"afternoon"},
 	];
-const weatherMatrix = [];
+const weatherTypes = ["snow", "rain", "wind", "sun", "heat"];
+const weatherMatrix = [
+	{
+		month:"January", 
+		typicalWeather:["snow", "rain", "wind"]
+	},
+	{
+		month:"December", 
+		typicalWeather:["snow", "rain", "wind"]
+	}
+	];
 const monthsOfYear = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 let currentMonth = "";
 let currentTimePeriod = "";
+let randomWeather = "";
+
 
 const findTimeOfDay = () => {
 	const now = new Date();
@@ -39,15 +52,32 @@ const getTimeBoundGreeting = () => {
 }
 
 const checkWeatherIsTypical = () => {
+	//get random weather
+	const weatherRandomNum = Math.floor(Math.random() * weatherTypes.length);
+	randomWeather = weatherTypes[weatherRandomNum];
+	//check if it is typcial for the month
+	console.log(randomWeather)
+	let weatherObj = weatherMatrix.filter((item)=> item["month"] === currentMonth)
+	console.log(JSON.stringify(weatherObj))
+	let isTypical = weatherObj[0]["typicalWeather"].indexOf(randomWeather);
 
+	return isTypical;
 }
 
 const generateMessage = () => {
 	
 	const greetingRandomNum = Math.floor(Math.random() * greetings.length);
-	console.log("tbg = " + getTimeBoundGreeting());
-	console.log(greetingRandomNum);
-	console.log(greetings[greetingRandomNum]);
+	const randomMessage = greetings[greetingRandomNum];
+	const timeBoundMessage = getTimeBoundGreeting();
+	const isTypicalWeather = checkWeatherIsTypical();
+
+	if (isTypicalWeather === -1) {
+		console.log(`${randomMessage}, ${timeBoundMessage}, this ${randomWeather} is crazy for this time of year isn't it!`);
+	} else {
+		console.log(`${randomMessage}, ${timeBoundMessage}, I hate this time of year, too much ${randomWeather}`);	
+	}
+	
+	
 }
 
 
